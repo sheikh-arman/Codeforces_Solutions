d0fc01de82a8677dbe3a3cd3800ac2ebf002ed4b
GitHub Action <action@github.com>
2023-07-08 01:49:11 +0000

    CF-AC-Submissions

---
+++ sk_arman/contest/1846B.cpp
@@ -0,0 +1,90 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem :
+    Solution :
+    Date:
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n;
+        string s[4];
+        for(ll i=0; i<3; i++)
+        {
+            cin>>s[i];
+        }
+        ll ck=1;
+        for(ll i=0; i<3&&ck; i++)
+        {
+            if(s[i][0]==s[i][1]&&s[i][1]==s[i][2]&&s[i][0]!='.')
+            {
+                cout<<s[i][0]<<"\n";
+                ck=0;
+                break;
+            }
+            if(s[0][i]==s[1][i]&&s[1][i]==s[2][i]&&s[0][i]!='.')
+            {
+                cout<<s[0][i]<<"\n";
+                ck=0;
+                break;
+            }
+        }
+        if(ck)
+        {
+            if(s[0][0]==s[1][1]&&s[1][1]==s[2][2]&&s[0][0]!='.')
+            {
+                cout<<s[0][0]<<"\n";
+                ck=0;
+            }
+            else if(s[0][2]==s[1][1]&&s[1][1]==s[2][0]&&s[1][1]!='.')
+            {
+                cout<<s[1][1]<<"\n";
+                ck=0;
+            }
+        }
+        if(ck)
+        {
+            cout<<"DRAW\n";
+        }
+
+
+    }
+    return 0;
+///*****************************  ALHAMDULILLAH  *****************************/
+}
